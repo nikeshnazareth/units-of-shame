74621340142516cd9d94a13ccf65d6d493314a14
Nikesh Nazareth <business@nikeshnazareth.com>
2022-11-22 00:34:32 +1100

    Get user-specific XP

--- src/proxy/server.js
+++ src/proxy/server.js
@@ -1,10 +1,21 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const Duolingo = require("duolingo-api");
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get("/user/:username", async (req, res) => {
+  const credential = {
+    username: req.params.username,
+  };
+  const duoAPI = new Duolingo(credential);
+  const result = await duoAPI.getRawData();
+  const user = result.users[0];
+  const status = {
+    name: user.name,
+    username: user.username,
+    XP: user.totalXp,
+  };
+  res.send(status);
 });
