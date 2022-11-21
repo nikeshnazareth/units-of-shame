f5da287f3da1ca0a788cba83b95a76f064d6a5b1
Nikesh Nazareth <business@nikeshnazareth.com>
2022-11-22 01:20:12 +1100

    Use proxy server for Duolingo results

--- src/DuolingoContext.tsx
+++ src/DuolingoContext.tsx
@@ -1,6 +1,7 @@
 import React, { Component, createContext } from "react";
 import { User } from "./types/user";
-const Duolingo = require("duolingo-api");
+
+const baseURL = "https://duoproxy.nfshost.com/user";
 
 interface IProps {
   children: React.ReactNode;
@@ -21,23 +22,8 @@ export class DuolingoProvider extends Component<IProps, IState> {
   getXP = (): Promise<User[]> =>
     Promise.all(
       this.state.competitors
-        .map((username) => ({ username }))
-        .map((credential) => new Duolingo(credential))
-        // not sure why getDataByFields doesn't work or why getRawData returns a list of users
-        // even though there is only one user in the credential. This should be cleaned up
-        .map((duoAPI) =>
-          duoAPI
-            .getRawData()
-            .then((result: any) => result.users[0])
-            .then(
-              (u: any) =>
-                ({
-                  name: u.name,
-                  username: u.username,
-                  XP: u.totalXp,
-                } as User)
-            )
-        )
+        .map((username) => `${baseURL}/${username}`)
+        .map((url) => fetch(url).then((res) => res.json()))
     );
 
   render() {
