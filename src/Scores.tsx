e3ed763795095d76f16b9e6a2ba08e7558e0fa6a
Nikesh Nazareth <business@nikeshnazareth.com>
2022-11-22 22:06:44 +1100

    Use leader as reference

--- src/Scores.tsx
+++ src/Scores.tsx
@@ -1,6 +1,12 @@
-import { Container, Card, Stack, Typography, Avatar } from "@mui/material";
+import {
+  Container,
+  Card,
+  Stack,
+  Typography,
+  Avatar,
+  Icon,
+} from "@mui/material";
 import { useContext, useState, useEffect } from "react";
-
 import { DuolingoContext } from "./DuolingoContext";
 import { User } from "./types/user";
 
@@ -8,18 +14,24 @@ const Scores = () => {
   const { getXP, getCompetitors } = useContext(DuolingoContext);
   const [usersXP, setUsersXP] = useState<User[]>([]);
 
+  const progress = (user: User) => user.XP - user.baseline;
+  // the first user (leader) is the reference
+  const shortfall = (user: User) => progress(usersXP[0]) - progress(user);
+  const shame = (user: User) => {
+    const s = shortfall(user);
+    return (s > 10000 ? 1 : 0) + Math.max(Math.floor((s - 10000) / 5000), 0);
+  };
+
   useEffect(() => {
     Promise.all(getCompetitors().map(getXP))
-      .then((users) =>
-        users.sort((a, b) => b.XP - b.baseline - (a.XP - a.baseline))
-      )
+      .then((users) => users.sort((a, b) => progress(b) - progress(a)))
       .then(setUsersXP);
   }, [getXP, getCompetitors]);
 
   return (
     <Container style={{ padding: "50px" }}>
       <Stack spacing={2} style={{ alignItems: "center" }}>
-        {usersXP.map((u) => (
+        {usersXP.map((u, idx) => (
           <Card key={u.username} style={{ width: 300, padding: 20 }}>
             <Container
               style={{
@@ -37,9 +49,17 @@ const Scores = () => {
             </Container>
             <Typography variant="subtitle1">Total: {u.XP}</Typography>
             <Typography variant="subtitle1">Baseline: {u.baseline}</Typography>
-            <Typography variant="h6" style={{ color: "green" }}>
-              +{u.XP - u.baseline}
-            </Typography>
+            <Typography variant="subtitle1">Progress: {progress(u)}</Typography>
+            {shortfall(u) > 0 ? (
+              <Typography
+                variant="h6"
+                style={{ color: shame(u) ? "red" : "orange" }}
+              >
+                -{shortfall(u)}
+              </Typography>
+            ) : null}
+            {/* replace the icon with ThumbsDownIcon */}
+            { Array(shame(u)).map((_, i) => (<Icon key={i}></Icon>)) }
           </Card>
         ))}
       </Stack>
